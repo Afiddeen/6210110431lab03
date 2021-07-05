e4593c6b4c106b3b9fd9df096970a9d695b5784b
6210110431lab03 <6210110431@psu.ac.th>
2021-07-05 13:47:14 +0700

    Part 5

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,8 +1,13 @@
-import React from ("react");
-export default function WordCard(props) {
-    return(
+import React from 'react';
+import CharacterCard from './CharacterCard';
+export default function WordCard(props){
+    const activationHandler = c =>{
+            console.log(`${c}has been activated`)
+    }
+    return (
         <div>
-            {Array.from(props.value).map((c,i)=> <CharacterCarderCard value={c} key={i}/>)}
+            { Array.from(props.value).map((c, i) => 
+                <CharacterCard value={c} key={i} activationHandler={activationHandler}/>) }
         </div>
-    )
+    );
 }
