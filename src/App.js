1044e434560449b99cfbfd8a479ac8db258af61a
6210110431 <6210110431@psu.ac.th>
2021-07-12 01:49:30 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
-const word = "Hello";
+
+
 function App() {
+  const word =["SHOES","SHIRT","SCARF","SUIT","SOCKS","HAT"] ;
+  const newGame = () => {
+    window.location.reload()
+  }
+
   return (
-    <div>
-      <WordCard value="hello"/>
+    <div className = "head">
+      <header>
+        <h1>Word Card</h1>
+      </header>
+      <div className = "main">
+        <h1>
+          สิ่งของที่สวมใส่เพื่อบดบังแดด
+        </h1>
+        <div>
+          <WordCard value = {"HAT"}/>
+        </div>
+        <h2>
+          ป้องกันอันตรายอันเกิดจากการสัมผัสกับพื้นผิวต่างๆ หรือการเจ็บเท้าจากการเดินหรือ การวิ่ง
+        </h2>
+        <div>
+          <WordCard value = {"SHOES"}/>
+        </div>
+      </div>
+        
+      <button className = "nemgame" onClick = {newGame}>
+        New
+      </button>
+      
     </div>
-    );
+
+    
+    
+  );
    
 }
 
